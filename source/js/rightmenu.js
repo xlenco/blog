3a0de4815250c43476f45f1d1f93b7c12332ef4a
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-08-22 21:13:08 +0800

    Update rightmenu.js

--- source/js/rightmenu.js
+++ source/js/rightmenu.js
@@ -90,6 +90,7 @@ rmf.switchReadMode = function () {
 //复制选中文字
 rmf.copySelect = function () {
     document.execCommand('Copy', false, null);
+    btf.snackbarShow('已复制！！');
     //这里可以写点东西提示一下 已复制
 }
 
