7c014756cfd2b3a969195d3939a5be77d8e3ef97
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-09-03 21:08:46 +0800

    Update 51la.js

--- source/js/51la.js
+++ source/js/51la.js
@@ -1,4 +1,4 @@
 //51.1a
 LA.init({ id: "JmsRdzV6syTLtUyn", ck: "JmsRdzV6syTLtUyn", hashMode: true });
 //灵雀应用监控
-new LingQue.Monitor().init({ id: "JnkoLfdriTFL8Kgt", sendSpaPv: true });
+new LingQue.Monitor().init({ id: "JntPZhhCgIfMtOhJ", sendSpaPv: true });
