bd2944f5102505348a009a98b240e40aaf6b78b3
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-09-11 17:15:03 +0800

    Update custom.js

--- source/js/custom.js
+++ source/js/custom.js
@@ -135,3 +135,10 @@ function percent() {
         btn.childNodes[0].style.display = 'block'
     }
 }
+
+// 分享本页
+function share() {
+    let url = window.location.origin + window.location.pathname
+    new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
+    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+}
