e262b4cd6ae5c6e955b61cac373b856a65522ce0
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-09-24 07:59:58 +0800

    Update custom.js

--- source/js/custom.js
+++ source/js/custom.js
@@ -139,4 +139,8 @@ function percent() {
         result<=99||(result=99),btn.innerHTML=result
 }
 
-document.getElementById("page-name").innerText=document.title.split(" | 安知鱼")[0];
+document.getElementById("page-name").innerText=document.title.split(" |Xlenco")[0];
+
+// cardLink
+cardLink(document.querySelectorAll('article a[target=_blank]'))
+
