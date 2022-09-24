2567620b825ff967044bf2243091e4aadee3fc10
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-09-24 21:22:31 +0800

    Update text.js

--- scripts/text.js
+++ scripts/text.js
@@ -1,8 +1,16 @@
-(
+/ *随机文章 /
+
+hexo.extend.generator.register('text', function (locals) {
+  const config = hexo.config.text || {}
+  const posts = []
+  for (const post of locals.posts.data) {
+    if (post.random !== false) posts.push(post.path)
+  }
+  return {
+    path: config.path || 'text/index.html',
     function(){
         fetch("https://fcircle.xlenco.eu.org/randomfriend").then(res => res.json()).then(res => {
             randomfriend_a = document.getElementById("randomfriend_a")
             randomfriend_a.harf = res["link"]
-        })
-    }
-)()
+  }
+})
