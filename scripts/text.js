94088d96bf3b3ea69bdccd1c45d3010a55b4053f
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-09-24 22:01:25 +0800

    Delete text.js

--- scripts/text.js
+++
@@ -1,9 +0,0 @@
-/ *随机文章 /
-
-hexo.extend.generator.register('text', function (locals) {
-  const config = hexo.config.text || {}
-  return {
-    path: config.path || 'text/index.html',
-    data: `<html><head><script>(    function(){        fetch("https://fcircle.xlenco.eu.org/randomfriend").then(res => res.json()).then(res => {            randomfriend_a = document.getElementById("randomfriend_a")            randomfriend_a.harf = res["link"]        })    })()`<html><head><script>
-  }
-})
