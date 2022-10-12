9afd71c25d0fa14c2593786fc6f5b56f879b84de
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-10-12 20:49:26 +0800

    Update custom.js

--- source/js/custom.js
+++ source/js/custom.js
@@ -123,24 +123,8 @@ function welcome_mes() {
 function share() {
     let url = window.location.origin + window.location.pathname
     new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
-    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~").innerText=document.title.split(" |Xlenco")[0];
 }
-
-
-// 返回顶部 显示网页阅读进度
-window.onscroll = percent;// 执行函数
-// 页面百分比
-function percent() {
-    let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
-        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
-        result = Math.round(a / b * 100), // 计算百分比
-        btn = document.querySelector("#percent"); // 获取图标
-
-        result<=99||(result=99),btn.innerHTML=result
-}
-
-document.getElementById("page-name").innerText=document.title.split(" |Xlenco")[0];
-
 // cardLink
 cardLink.server = 'https://x.abudu.top/?url='
 cardLink(document.querySelectorAll('article a[target=cardlink]'))
