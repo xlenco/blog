704b67133ebf1cc10c843999bc257e5fb2b47210
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-12-08 20:08:55 +0800

    Update modify.js

--- scripts/modify.js
+++ scripts/modify.js
@@ -3,3 +3,4 @@ const { filter } = hexo.extend;
 
 const js = hexo.extend.helper.get('js').bind(hexo);
 hexo.extend.injector.register('head_begin', () => {return js('/js/clientworker.js');});
+hexo.extend.injector.register('head_begin', () => {return js('https://npm.elemecdn.com/jquery@3.2.1/dist/jquery.min.js');});
