ce207ca8904bccda4447d37098e0a42e09e754ca
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-12-19 10:17:51 +0800

    Update modify.js

--- scripts/modify.js
+++ scripts/modify.js
@@ -3,5 +3,4 @@ const { filter } = hexo.extend;
 
 const js = hexo.extend.helper.get('js').bind(hexo);
 hexo.extend.injector.register('head_begin', () => {return js('/js/clientworker.js');});
-const css = hexo.extend.helper.get('css').bind(hexo);
-hexo.extend.injector.register('head_begin', () => {return css('/css/_custom/custom.css');});
+
