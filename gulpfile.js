2d258adee5dde3a1461329082e9a25ca8a0ea1ec
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-12-21 07:58:28 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
 // gulp-tester
 var gulp = require('gulp');
+// PWA
+var terser = require('workbox-build');
+gulp.task("generate-service-worker", () => {
+  return workbox.injectManifest({
+    swSrc: "./sw-template.js",
+    swDest: "./public/sw.js",
+    globDirectory: "./public",
+    globPatterns: [
+      // 缓存所有以下类型的文件，极端不推荐
+      // "**/*.{html,css,js,json,woff2,xml}"
+      // 推荐只缓存404，主页和主要样式和脚本。
+      "404.html",
+      "index.html",
+      "js/main.js",
+      "css/index.css",
+    ],
+    modifyURLPrefix: {
+      "": "./",
+    },
+  });
+});
 // 压缩js
 var terser = require('gulp-terser');
 gulp.task('compress', () =>
@@ -64,6 +85,6 @@ gulp.task('mini-font', (cb) => {
     });
 });
 // 运行gulp命令时依次执行以下任务
-gulp.task('default', gulp.parallel(
+gulp.task('default',gulp.series('generate-service-worker',gulp.parallel(
   'compress','minify-css','minify-html','mini-font'
-))
+)));
