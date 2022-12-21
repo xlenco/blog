a9854d74f7dedeffd40c86a08496050e3832fa26
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-12-21 15:19:00 +0800

    Update custom.js

--- source/js/custom.js
+++ source/js/custom.js
@@ -870,3 +870,12 @@ var CountUp = function (target, startVal, endVal, decimals, duration, options) {
     self.printValue(self.startVal);
   }
 };
+
+//重定向浏览器地址
+pjax.site_handleResponse = pjax.handleResponse;
+pjax.handleResponse = function(responseText, request, href, options){
+  Object.defineProperty(request,'responseURL',{
+    value: href
+  });
+  pjax.site_handleResponse(responseText,request,href,options);
+}
