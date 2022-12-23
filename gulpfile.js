fe17f66c2fa37f50f5906a9e15162343901093f1
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-12-23 20:41:15 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,26 @@ gulp.task('minify-css', () => {
         }))
         .pipe(gulp.dest('./public'));
 });
+// 压缩 public 目录内 html
+gulp.task("minify-html", () => {
+  return gulp
+    .src("./public/**/*.html")
+    .pipe(htmlclean())
+    .pipe(
+      htmlmin({
+        removeComments: true, //清除 HTML 註释
+        collapseWhitespace: true, //压缩 HTML
+        collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true, //删除 <script> 的 type="text/javascript"
+        removeStyleLinkTypeAttributes: true, //删除 <style> 和 <link> 的 type="text/css"
+        minifyJS: true, //压缩页面 JS
+        minifyCSS: true, //压缩页面 CSS
+        minifyURLs: true,
+      })
+    )
+    .pipe(gulp.dest("./public"));
+});
 //压缩字体
 var fontmin = require('gulp-fontmin');
 function minifyFont(text, cb) {
@@ -42,5 +62,5 @@ gulp.task('mini-font', (cb) => {
 });
 // 运行gulp命令时依次执行以下任务
 gulp.task('default', gulp.parallel(
-  'compress','minify-css','mini-font'
+  'compress','minify-css','minify-html','mini-font'
 ))
