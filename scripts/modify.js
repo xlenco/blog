4bfe8c0648e594056b64e7b08298b569482865c9
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-12-26 21:26:15 +0800

    Create modify.js

---
+++ scripts/modify.js
@@ -0,0 +1,5 @@
+'use strict';
+const { filter } = hexo.extend;
+
+const js = hexo.extend.helper.get('js').bind(hexo);
+hexo.extend.injector.register('head_begin', () => {return js('/js/load-awesome.js');});
