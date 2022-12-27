7dc951404912bdaea37b4b4a4a82bc8d4bfa7317
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-12-27 18:24:46 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 const workbox = require("workbox-build");
 gulp.task('generate-service-worker', () => {
     return workbox.injectManifest({
-        swSrc: './sw-template.js',
+        swSrc: './workbox-sw.js',
         swDest: './public/sw.js',
         globDirectory: './public',
         globPatterns: [
