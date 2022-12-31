45c9b527c528be1b1e60a9aeece03c25e8899d52
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-12-31 09:43:40 +0800

    Update workbox-sw.js

--- workbox-sw.js
+++ workbox-sw.js
@@ -74,7 +74,19 @@ workbox.routing.registerRoute(
         ]
     })
 );
-
+// 静态资源
+workbox.routing.registerRoute(
+    new RegExp('.*.(?:css|js)'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: '静态资源',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 50,
+                maxAgeSeconds: WEEK
+            }),
+        ]
+    })
+);
 
 // 离线谷歌分析
 // workbox.googleAnalytics.initialize();
