941a5e66d15407f9c9387a909a4fa10a0561e88c
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-01-02 17:42:13 +0800

    Update workbox-sw.js

--- workbox-sw.js
+++ workbox-sw.js
@@ -128,5 +128,13 @@ const configs = {
                 '_',
             ],
         },
+        {
+            'rule': /^https\:\/\/text\.text\.cn\/gh)/,
+            'search': '_',
+            'replace': [
+                'https://jsd.onmicrosoft.cn/gh',
+                '_',
+            ],
+        },
     ],
 }
