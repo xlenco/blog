163d0fae6e04aa6337cb53ac1e250d1bc3630162
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-01-02 11:45:40 +0800

    Update workbox-sw.js

--- workbox-sw.js
+++ workbox-sw.js
@@ -90,3 +90,49 @@ workbox.routing.registerRoute(
 
 // 离线谷歌分析
 // workbox.googleAnalytics.initialize();
+
+const configs = {
+    'cdn': [
+        {
+            'rule': /^https\:\/\/((cdn|fastly|gcore|test1|quantil)\.jsdelivr\.net\/npm|unpkg\.com)/,
+            'search': '_',
+            'replace': [
+                'https://jsd.cxl2020mc.top/npm',
+                'https://vercel.jsd.cxl2020mc.top/npm',
+                'https://jsdcxl2020mc.domain.qystu.cc/npm',
+                'https://unpkg.cnortles.top',
+                'https://cdn.cnortles.top/npm',
+                'https://jsdelivr.pai233.top/npm',
+                'https://cdn.jsdelivr.net/npm',
+                'https://cdn2.tianli0.top/npm',
+                'https://cdn3.tianli0.top/npm',
+                // qycdn
+                'https://cdn.chuqis.com/npm',
+                'https://jsd.cky.codes/npm',
+                'https://unpkg.com',
+                '_',
+            ],
+        },
+        {
+            'rule': /^https\:\/\/((cdn|fastly|gcore|test1|quantil)\.jsdelivr\.net\/gh)/,
+            'search': '_',
+            'replace': [
+                'https://jsd.cxl2020mc.top/gh',
+                'https://vercel.jsd.cxl2020mc.top/gh',
+                'https://jsdcxl2020mc.domain.qystu.cc/gh',
+                'https://cdn.cnortles.top/gh',
+                'https://jsdelivr.pai233.top/gh',
+                'https://cdn.jsdelivr.net/gh',
+                'https://cdn2.tianli0.top/gh',
+                'https://cdn3.tianli0.top/gh',
+                // qycdn
+                'https://cdn.chuqis.com/gh',
+                'https://jsd.cky.codes/gh',
+                '_',
+            ],
+        },
+    ],
+}
+
+
+importScripts('/js/sw-dev.js')
