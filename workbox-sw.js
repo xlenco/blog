e3bd316a3b85100051e099c4e1fd604c96069ec9
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-01-02 17:27:04 +0800

    Update workbox-sw.js

--- workbox-sw.js
+++ workbox-sw.js
@@ -1,5 +1,5 @@
 importScripts('https://jsd.onmicrosoft.cn/npm/workbox-sw/build/workbox-sw.js');
-importScripts('https://jsd.onmicrosoft.cn/npm/clientworker@latest')
+
 
 
 if (workbox) {
@@ -91,40 +91,3 @@ workbox.routing.registerRoute(
 
 // 离线谷歌分析
 // workbox.googleAnalytics.initialize();
-
-
-// cw 安装
-if (!!navigator.serviceWorker) {
-    if (localStorage.getItem('cw_installed') !== 'true') {window.stop();}
-    navigator.serviceWorker.register('/cw.js?t=' + new Date().getTime()).then(async (registration) => {
-        if (localStorage.getItem('cw_installed') !== 'true') {
-            const conf = () => {
-                console.log('[CW] Installing Success,Configuring...');
-                fetch('/cw-cgi/api?type=config')
-                    .then(res => res.text())
-                    .then(text => {
-                        if (text === 'ok') {
-                            console.log('[CW] Installing Success,Configuring Success,Starting...');
-                            localStorage.setItem('cw_installed', 'true');
-                            window.location.reload();
-                        } else {
-                            console.warn('[CW] Installing Success,Configuring Failed,Sleeping 200ms...');
-                            setTimeout(() => {
-                                conf()
-                            }, 200);
-                        }
-                    }).catch(err => {
-                        console.log('[CW] Installing Success,Configuring Error,Exiting...');
-                    });
-            }
-            setTimeout(() => {
-                conf()
-            }, 50);
-        }
-    }).catch(err => {
-        console.error('[CW] Installing Failed,Error: ' + err.message);
-    })
-} else { console.error('[CW] Installing Failed,Error: Browser not support service worker'); }
-
-
-
