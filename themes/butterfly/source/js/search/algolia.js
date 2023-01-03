9c80461bf2c06446cb3f8b0a908cd62ea0566f9e
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-01-03 08:35:06 +0800

    Update algolia.js

--- themes/butterfly/source/js/search/algolia.js
+++ themes/butterfly/source/js/search/algolia.js
@@ -82,6 +82,8 @@ window.addEventListener('load', () => {
     container: '#algolia-search-input',
     showReset: false,
     showSubmit: false,
+    showSubmit: true, // 设为true 可以通过按钮搜索
+    searchAsYouType: true, // 新增 可以实现回车或点击按钮搜索，不会每次输入都
     placeholder: GLOBAL_CONFIG.algolia.languages.input_placeholder,
     showLoadingIndicator: true
   })
