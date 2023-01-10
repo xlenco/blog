5f13f055469ee814d6e14b5318d6aae34932bbe9
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-01-10 15:21:57 +0800

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="xlenco.vercel.app";
+        url.hostname="xlenco.eu.org";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
