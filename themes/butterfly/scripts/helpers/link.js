e613646fa984dce8218e6a0edec599942e4bf1bf
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-02-04 17:17:53 +0800

    Update link.js

--- themes/butterfly/scripts/helpers/link.js
+++ themes/butterfly/scripts/helpers/link.js
@@ -8,5 +8,5 @@ data.forEach((e, i) => {
     let j = data.length-1;
     if (i != j) ls = ls.concat(e.link_list) 
 }); 
-fs.writeFileSync('source/link.json', `[{"link_list": ${JSON.stringify(ls)},"length":${ls.length}}]`) 
+fs.writeFileSync('source/link.json', `{"link_list": ${JSON.stringify(ls)},"length":${ls.length}}`) 
 console.log('友链文件已生成。'); 
