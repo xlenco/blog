e2c19da633b45646c5cf6d4b724b477ffeeceaca
Xlenco <75837117+xlenco@users.noreply.github.com>
2024-02-13 10:06:47 +0800

    Create install_cw.js

---
+++ scripts/install_cw.js
@@ -0,0 +1,4 @@
+'use strict';
+const { filter } = hexo.extend;
+const js = hexo.extend.helper.get('js').bind(hexo);
+hexo.extend.injector.register('head_begin', () => {return js('/js/install_cw.js');});
