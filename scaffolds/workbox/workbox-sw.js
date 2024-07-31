6bb848a9ddc59aa5ef5298e857631dbf95c1b252
xlenco <1043865083@qq.com>
2024-07-31 20:18:34 +0800

    - 更换npm cdn - 添加sw

--- scaffolds/workbox/workbox-sw.js
+++ scaffolds/workbox/workbox-sw.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.cbd.int/workbox-sw/build/workbox-sw.js");
+importScripts("https://npm.elemecdn.com/workbox-sw/build/workbox-sw.js");
 
 // 提示
 if (workbox) {
@@ -160,12 +160,6 @@ const Offline = new workbox.routing.Route(
     })
 );
 workbox.routing.registerRoute(Offline);
-
-// busuanzi 请求走网络不缓存
-workbox.routing.registerRoute(
-    ({ url }) => String(url).includes('busuanzi?') || String(url).includes('busuanzi='),
-    new workbox.strategies.NetworkOnly()
-);
 
 // 缓存静态资源
 workbox.routing.registerRoute(
